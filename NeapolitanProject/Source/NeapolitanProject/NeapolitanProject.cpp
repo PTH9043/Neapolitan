853b947282a4a9e55a1a88184d879af71b27f90f
Park Tae Heon <my5827@naver.com>
2026-02-03 12:35:41 +0900

    언리얼 5.6으로 시작
    폴더를 나누어놓음.
    모든 에셋 폴더는 MarketPlace에 놓기.

---
+++ NeapolitanProject/Source/NeapolitanProject/NeapolitanProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NeapolitanProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NeapolitanProject, "NeapolitanProject" );
